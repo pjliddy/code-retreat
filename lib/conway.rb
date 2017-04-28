5477308c733b130ad512982a1fa83ca9852359d4
pjliddy <pjliddy@gmail.com>
2017-04-27 23:08:37 -0400

    Fully functional game of Life
    Optimized Ruby with no linter errors. New Game objects have number
    of rows and columns as arguments, along with seed patterns: 'acorn',
    'pento', 'rabbit', or a percentage from 1 to 100 for the density of a
    random pattern. x = Game.new(rows, cols, {'acorn', 'pento', 'rabbit',
    1..100}).

--- lib/conway.rb
+++ lib/conway.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
-require 'pry'
+# require_relative 'lib/conway.rb'
+# x = Game.new(cols, rows ,'acorn | pento | rabbit | random_%').play
+# x = Game.new(40,30,'acorn').play
+
+# x.play
 
 # Game class
 class Game
@@ -9,48 +13,74 @@ class Game
     @cols = cols
     @rows = rows
     @board = Array.new(@rows) { Array.new(@cols, 0) }
-
-    init_seeds
-    init_pattern(mode)
-    check_board
-  end
-
-  # initialize pattern seeds
-  def init_seeds
-    @acorn = [[-1, -2], [0, 0], [1, -3], [1, -2], [1, 1], [1, 2], [1, 3]]
-    @pento = [[-1, 0], [-1, 1], [0, -1], [0, 0], [1, 0]]
-    @rabbit = [[-1, -3], [0, -3], [0, -2], [0, -1], [1, -2], [1, 1],
-               [1, 2], [1, 3], [0, 2]]
+    init_board(mode)
+    self
   end
 
   # initialize board with specified pattern
+  def init_board(mode)
+    if mode.is_a? String
+      # draw the pattern
+      init_pattern(mode)
+    elsif mode.is_a? Numeric
+      # seed with a random pattern of mode %
+      fill_random(mode)
+    else
+      # seed with a random pattern of 25 %
+      fill_random(25)
+    end
+  end
+
+  # render one of the pre-defined patterns
   def init_pattern(mode)
     if mode == 'acorn'
-      pattern_init(@acorn)
+      fill_pattern([[-1, -2], [0, 0], [1, -3], [1, -2], [1, 1], [1, 2], [1, 3]])
     elsif mode == 'pento'
-      pattern_init(@pento)
+      fill_pattern([[-1, 0], [-1, 1], [0, -1], [0, 0], [1, 0]])
     elsif mode == 'rabbit'
-      pattern_init(@rabbit)
+      fill_pattern([[-1, -3], [0, -3], [0, -2], [0, -1], [1, -2], [1, 1],
+                    [1, 2], [1, 3], [0, 2]])
     else
-      # seed with a random pattern
-      random_init(25)
+      fill_random(25)
     end
   end
 
   # fill in cells with seed pattern
-  def pattern_init(cells)
+  def fill_pattern(cells)
     c = @cols / 2
     r = @rows / 2
     cells.each { |cell| @board[cell[0] + r][cell[1] + c] = 1 }
   end
 
   # initialize the board with p% random cells
-  def random_init(p)
+  def fill_random(percent)
     @board.each_with_index do |r, ri|
       r.each_with_index do |_c, ci|
-        @board[ri][ci] = rand(100) > p ? 0 : 1
+        # set some percent of cells to 1 (live)
+        @board[ri][ci] = rand(100) > percent ? 0 : 1
       end
     end
+  end
+
+  # loop through the board until a done state exists
+  def play
+    done = false
+    done = check_board until done
+  end
+
+  # check the status of each cell on the board
+  def check_board
+    # @last = @board
+    done = false
+    draw_board
+    turn = Array.new(@rows) { Array.new(@cols, 0) }
+    turn.each_with_index do |r, ri|
+      # check new status for each cell and store in next_board array
+      r.each_with_index { |_c, ci| turn[ri][ci] = check_square(ri, ci) }
+    end
+
+    turn == @board ? done = true : @board = turn
+    done
   end
 
   # draw the game board for each turn
@@ -72,19 +102,18 @@ class Game
     print "\n"
   end
 
-  # check the status of each cell on the board
-  def check_board
-    done = false
+  # check the number of neighbors for a cell
+  def check_square(r, c)
+    n = 0
+    bounds = get_bounds(r, c)
 
-    until done
-      draw_board
-      next_board = Array.new(@rows) { Array.new(@cols, 0) }
-      next_board.each_with_index do |r, ri|
-        r.each_with_index { |_c, ci| next_board[ri][ci] = check_square(ri, ci) }
+    (bounds[:r_min]..bounds[:r_max]).each do |ri|
+      (bounds[:c_min]..bounds[:c_max]).each do |ci|
+        n += @board[ri][ci] unless ri == r && ci == c
       end
-
-      next_board == @board ? done = true : @board = next_board
     end
+
+    live_or_die(n, r, c)
   end
 
   # get the min/max bounds of cells to check on edges of the game board
@@ -97,21 +126,6 @@ class Game
     }
   end
 
-  # check the number of neighbors for a cell
-  def check_square(r, c)
-    # initialize number of neibhors
-    n = 0
-    bounds = get_bounds(r, c)
-
-    (bounds[:r_min]..bounds[:r_max]).each do |row|
-      (bounds[:c_min]..bounds[:c_max]).each do |col|
-        n += @board[row][col] unless row == r && col == c
-      end
-    end
-
-    live_or_die(n, r, c)
-  end
-
   # check whether the cell lives or dies
   def live_or_die(n, row, col)
     dead = @board[row][col].zero?
@@ -119,8 +133,7 @@ class Game
   end
 end
 
-# x = Game.new(cols, rows ,'acorn | pento | rabbit | random')
-# x = Game.new(88,46,'random')
-# x = Game.new(40,30,'rabbit')
-binding.pry
-''
+# require_relative 'lib/conway.rb'
+puts "x = Game.new(cols, rows ,'acorn | pento | rabbit | random_%').play"
+# x = Game.new(88,46,25).play
+# x = Game.new(40,30,'rabbit').play
